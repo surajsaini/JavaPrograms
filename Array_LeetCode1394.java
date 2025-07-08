c2834074d588bc3f9a928587f577cbe9a5b179a7
Suraj Saini <s2008saini@gmail.com>
2025-07-08 13:38:34 -0700

    leetcode1394

---
+++ Array_LeetCode1394.java
@@ -0,0 +1,32 @@
+package JavaPrograms;
+
+import java.util.*;
+public class Array_LeetCode1394 {
+	// 1394. Find Lucky Integer in an Array
+	
+	public int findLucky(int[] arr) {
+
+		int count2 = 0, lucky = -1;
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			int count = 0;
+			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+		}
+		for (int i : map.keySet()) {
+			if (i == map.get(i)) {
+				if (lucky < map.get(i)) {
+					lucky = map.get(i);
+				}
+			}
+		}
+
+		return lucky;
+	}
+
+	
+	public static void main(String[] args) {
+
+		findLucky([2,2,3,4]);
+	}
+}
